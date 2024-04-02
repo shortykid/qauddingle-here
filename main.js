816f4c9ebe3266d03e7350fe7f4cdc26fe827861
shortykid <140464830+shortykid@users.noreply.github.com>
2024-04-01 19:26:44 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
 song = loadSound("music.mp3")
@@ -11,6 +15,13 @@ function setup(){
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized')
 }
 
 function draw(){
@@ -18,4 +29,20 @@ function draw(){
 }
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX  =results[0].pose.leftWrist.x;
+        leftWristY  =results[0].pose.leftWrist.y;
+        console.log("leftWristX = "+ leftWristX + "leftWritY = "+ leftwristY);
+        rightWristX  =results[0].pose.rightWrist.x;
+        rightWristY  =results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+ rightWristX + "rightWritY = "+ rightwristY);
+    }
 }
