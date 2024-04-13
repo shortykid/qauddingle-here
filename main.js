c5c167e0d216ac4a1e469e9a0edbbd415e59fd05
shortykid <140464830+shortykid@users.noreply.github.com>
2024-04-12 19:33:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,48 +1,18 @@
-song = "";
-leftWristX = 0;
-leftWristY = 0;
-rightWristX = 0;
-rightWristY = 0;
-
-function preload(){
-song = loadSound("music.mp3")
-}
-
-
-function setup(){
-    canvas = createCanvas(600,500);
-    canvas.center();
-
-    video = createCapture(VIDEO);
-    video.hide();
-
-    poseNet = ml5.poseNet(video, modelLoaded)
-    poseNet.on('pose',gotPoses);
-}
-
-function modelLoaded(){
-    console.log('PoseNet Is Initialized')
-}
-
-function draw(){
-    image(video,0,0,600,500)
-}
-function play(){
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
-}
-
-function gotPoses(results)
+img="";
+function preload()
 {
-    if(results.length > 0)
-    {
-        console.log(results);
-        leftWristX  =results[0].pose.leftWrist.x;
-        leftWristY  =results[0].pose.leftWrist.y;
-        console.log("leftWristX = "+ leftWristX + "leftWritY = "+ leftwristY);
-        rightWristX  =results[0].pose.rightWrist.x;
-        rightWristY  =results[0].pose.rightWrist.y;
-        console.log("rightWristX = "+ rightWristX + "rightWritY = "+ rightwristY);
-    }
+    img = loadImage('dog_cat.jpg');
+}
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+function draw()
+{
+image(img, 0, 0, 640, 420);
+fill("#FF0000")
+text("Dog", 45, 75);
+noFill();
+stroke("#FF0000")
+rect(30, 60, 450, 350);
 }
